c90c061f6ae363fdf49b2003f69dd42aac439b8f
dragos-vilcescu <85735625+dragos-vilcescu@users.noreply.github.com>
2021-06-11 12:37:43 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
     organization = "infrastructure-pipelines-workshop"
 
     workspaces {
-      name = "john-d-vault"
+      name = "dragos-v-vault"
     }
   }
   required_providers {
